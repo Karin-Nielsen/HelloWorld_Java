bb60a3bcf66dd120b23192f0e2773ece6a631cf5
Karin Nielsen <74790359+Karin-Nielsen@users.noreply.github.com>
2021-04-28 10:37:13 -0500

    Add HelloWorld.java to repository

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * File name: 	HelloWorld.java
+ * Author: 		Karin Nielsen
+ * Date: 		08/25/2020
+ * Version: 	1.0
+ * Copyright: 	All Rights Reserved, 2020
+ * Description: 
+ * 				Simple Hello World java program. Prints "Hello World!" to the console.
+ * */
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+}
